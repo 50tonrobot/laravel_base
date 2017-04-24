e5610b5476a808166f7332e75c9e5635de49a4ac
50tonrobot <50tonrobot@gmail.com>
2017-04-24 15:29:29 -0400

    Adds movie table schema

---
+++ app/base/database/migrations/2017_04_24_191156_create_movie_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMovieTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('Title');
+            $table->enum('Format',['VHS','DVD','Streaming']);
+            $table->integer('Length');
+            $table->integer('Year');
+            $table->tinyInteger('Rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
