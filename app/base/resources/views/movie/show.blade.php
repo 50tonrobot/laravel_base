ccf6cbb5a2cd976010010c7134e3a4d1b5e6db5f
50tonrobot <50tonrobot@gmail.com>
2017-04-25 23:29:06 -0400

    Adds styling to the show page

--- app/base/resources/views/movie/show.blade.php
+++ app/base/resources/views/movie/show.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class='well col-sm-10 col-sm-offset-1 col-xs-12 container conatiner-fluid'>
-    <h1>{{ $movie->Title }}</h1>
-    <div>{{ $movie->Year }}</div>
-    <div>{{ $movie->Format }}</div>
-    <div>{{ $movie->Length }}</div>
-    <div>{{ $movie->Rating }}</div>
-    <div>{{ $movie->Format }}</div>
+  <div class='well col-sm-6 col-sm-offset-3 col-xs-12 container conatiner-fluid'>
+    <h1>{{ $movie->Title }} <a href='/movie' class='btn btn-sm btn-info pull-right'>Back</a></h1>
+    <h5 class="movie-title-label">Title</h5>
+    <div><label for="Year">Year:&nbsp;</label><span id="Year">{{ $movie->Year }}</span></div>
+    <div><label for="Rating">Rating:&nbsp;</label><span id="Rating">{{ $movie->Rating }}</span></div>
+    <div><label for="Length">Length:&nbsp;</label><span id="Length">{{ $movie->Length }}</span></div>
+    <div><label for="Format">Format:&nbsp;</label><span id="Format">{{ $movie->Format }}</span></div>
   </div>
 @endsection
