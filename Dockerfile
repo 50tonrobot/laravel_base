301fb44c190acc4a371b4193a587ddc3e827ecf2
50tonrobot <50tonrobot@gmail.com>
2017-04-30 21:40:59 -0400

    optimized build state

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,11 @@
 FROM php:7.1
-ENV \
-  APP_DIR="/app/base" \
-  APP_PORT="80"
-COPY ./app/. /app
-COPY config/php.ini /usr/local/etc/php
-
 RUN apt-get update && apt-get install -y \
     vim locate mysql-client \
     git zip unzip zlib1g-dev openssl libxml2-dev \
-    libmemcached-dev \
-    && docker-php-ext-install zip \
-    && docker-php-ext-install mbstring \
-    && docker-php-ext-install tokenizer \
-    && docker-php-ext-install xml \
-#    && pecl install memcached-2.2.0 \
-#    && docker-php-ext-enable memcached \
-    && docker-php-ext-install mysqli \
-    && docker-php-ext-install pdo_mysql
-COPY config/env /home/env
-COPY config/init.sh /usr/local/bin/init.sh
-COPY config/wait-for-mysql.sh /usr/local/bin/wait-for-mysql.sh
-RUN chmod +x /usr/local/bin/init.sh \
-   && chmod +x /usr/local/bin/wait-for-mysql.sh
+    && docker-php-ext-install zip mbstring tokenizer xml mysqli pdo_mysql \
+    && docker-php-ext-enable pdo_mysql mysqli
+
+CMD ["php", "-a"]
+
+#build this image
+# docker build -t laravel-ready:1 .
--- config/init.sh
+++ config/init.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');";
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;";
-php composer-setup.php;
-php -r "unlink('composer-setup.php');";
-mv composer.phar /usr/local/bin/composer;
-#cd /app
-#composer create-project --prefer-dist laravel/laravel base --no-plugins --no-scripts;
-rm -rf /app/base/.env.example
+
+#if composer is not found globally
+if [ ! -f /usr/local/bin/composer ]; then
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');";
+  php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;";
+  php composer-setup.php;
+  php -r "unlink('composer-setup.php');";
+  mv composer.phar /usr/local/bin/composer;
+fi
+
+#if .env.example does exist
+# probably means brand new installation
+if [ -f /app/base/.env.example ]; then
+  rm -rf /app/base/.env.example
+
+fi
+
+#always safe to see if env is fresh
 mv /home/env /app/base/.env
+
+# needs to be in /app base from this point forward
 cd /app/base
-composer update
-composer install
+
+# if no vendor directory, then needs installing
+if [ ! -d "/app/base/vendor" ]; then
+  composer update
+  composer install
+fi
+
+
+#always safe to create new key
 php artisan key:generate
-#php artisan make:auth
-php artisan migrate
+
+# if no vendor directory, then needs installing
+if [ ! -d "/app/base/vendor" ]; then
+  php artisan migrate
+fi
+
+#spin up server, always
 php artisan serve --host=0.0.0.0 --port=80
